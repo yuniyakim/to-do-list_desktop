bc8965e106242f9b4117efc8f116f77091979f17
Yuniya Kim <yuniyakim@gmail.com>
2024-05-19 13:56:26 +0300

    Add onRender

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import {useState, Profiler} from 'react';
 import {ScrollView, Text} from 'react-native';
 import {ToDoForm} from './src/components/to-do-form/to-do-form';
 import {ToDoList} from './src/components/to-do-list/to-do-list';
@@ -14,11 +14,27 @@ export const App = () => {
     return newToDos;
   };
 
+  const onRender = (_id: string, _phase: 'mount' | 'update', actualDuration: number, _baseDuration: number, _startTime: number, _commitTime: number) => {
+    console.log(actualDuration)
+    // const platform = 'macos';
+    // const url = 'http://192.168.67.1:5555/add';
+    // fetch(url, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Accept': 'application/json',
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({app: 'desktop', platform: platform, time: actualDuration, device: 'Apple MacBook Air'})
+    // });
+  }
+
   return (
-    <ScrollView contentContainerStyle={styles.toDo_list_app_container}>
-      <Text style={styles.toDo_list_app_title}>ToDo List</Text>
-      <ToDoForm addToDo={addToDo} />
-      <ToDoList toDos={toDos} setToDos={setToDos} />
-    </ScrollView>
+    <Profiler id="App" onRender={onRender}>
+      <ScrollView contentContainerStyle={styles.toDo_list_app_container}>
+        <Text style={styles.toDo_list_app_title}>ToDo List</Text>
+        <ToDoForm addToDo={addToDo} />
+        <ToDoList toDos={toDos} setToDos={setToDos} />
+      </ScrollView>
+    </Profiler>
   );
 };
